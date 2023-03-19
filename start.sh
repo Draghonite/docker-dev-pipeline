6e9dedefd63eeea71aa5b81187c813a1b683631c
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-03-18 23:29:37 -0400

    Removed problematic optional exported ports

--- start.sh
+++ start.sh
@@ -13,8 +13,6 @@ docker run \
     --volume jenkins-docker-certs:/certs/client \
     --volume jenkins-data:/var/jenkins_home \
     --publish 2376:2376 \
-    --publish 3000:3000 \
-    --publish 5000:5000 \
     docker:dind \
     --storage-driver overlay2 
 
